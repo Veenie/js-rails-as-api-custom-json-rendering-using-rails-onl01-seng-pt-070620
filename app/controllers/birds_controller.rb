ddbf75a8d9a3b226be71964912375d3fd0341ddc
Raveen K Sharma <raveen08@gmail.com>
2021-01-25 21:03:25 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,7 +1,10 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
+    birds = Bird.all
     render json: @birds
   end
-  
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
 end
